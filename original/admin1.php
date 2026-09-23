2cdfe10d8112a513943cda59d79615d9cf1cedf5
ㅤ <107886961+ErionNezha@users.noreply.github.com>
2026-09-24 00:23:06 +0200

    Publikimi fillestar — demo live + kodi burim

---
+++ original/admin1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Adminga kirish</title>
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body background="img/APP19_09-03-Spring-Break-Website-Hero-BG.jpg">
+	<div class="container">
+		
+	 <form action="admingakirish.php"method="post"class=" justify-content-center">
+                    <div class="col-md-10">
+                  <label>Username</label>
+                  <input type="text" name="username"class="form-control"required><br>
+                  <label>Password</label>
+                  <input type="text" name="password"class="form-control"required><br>
+                    </div>
+           <button type="submit" class="btn btn-primary form-control"> kiritish</button>
+              </form>
+	</div>
+</body>
+</html>
